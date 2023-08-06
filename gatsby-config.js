9eaf411e3a8729333a732b32e5676237daae68bc
Rene Nadorp <rene@rnadorp.nl>
2023-08-06 12:06:00 +0200

    github integration attempt

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,39 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
- */
+*/
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 module.exports = {
   siteMetadata: {
     title: `pkm`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  
+    plugins: [
+   {
+      resolve: `gatsby-source-git`,
+      options: {
+
+        apiKey: process.env.API_KEY,
+        repos: [
+          {
+            name: `pkm-gatsby`,
+            remote: `https://github.com/renenadorp/Knowledge.git`,
+            branch: `main`,
+            local: `/Users/rnadorp/Documents/Prive/pkm/gatsby/pkm`,
+
+            patterns: [`*.md`],
+          },          {
+            name: `pkm-gatsby`,
+            remote: `https://github.com/renenadorp/Knowledge.git`,
+            patterns: [`*.jpg`],
+            branch: `main`,
+            local: `/Users/rnadorp/Documents/Prive/pkm/gatsby/pkm`
+
+          },
+        ],
+      },
+    },
+  ]
 }
